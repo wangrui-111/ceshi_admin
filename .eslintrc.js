521987cc65e560aaaea970f63e58d309871601d6
已若空幽径 <3049419097@qq.com>
2021-11-15 19:10:13 +0800

    第一次提交

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,14 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
+  /*
+    线上：production 需要检测 --》npm run build -->console 
+    线下：devlopment
+  */
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'quotes':'off',
+    'space-before-function-paren':'off'
   }
 }
